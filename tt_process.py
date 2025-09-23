cf07b48aef263d3585da472f41254320443d4c5d
Edward0333 <876166923@qq.com>
2025-09-23 22:28:15 +0800

    .

--- tt_process.py
+++ tt_process.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-def get_ego_feature(car_group_df, dict_cars):
+def get_ego_feature(car_group_df, dict_cars, merged_num):
     T = len(car_group_df)
 
     position = np.zeros((T, 2), dtype=np.float64)
@@ -32,22 +32,37 @@ def get_ego_feature(car_group_df, dict_cars):
         lane_id[i] = car_group_df["laneId"].values[i]
 
     return {
-        "position": position,
-        "velocity": velocity,
-        "acceleration": acceleration,
-        "shape": shape,
-        "lane_id": lane_id,
-        "valid_mask": valid_mask,
+        "position": position[::int(merged_num)],
+        "velocity": velocity[::int(merged_num)],
+        "acceleration": acceleration[::int(merged_num)],
+        "shape": shape[::int(merged_num)],
+        "lane_id": lane_id[::int(merged_num)],
+        "valid_mask": valid_mask[::int(merged_num)],
         "class": car_class,
         "drivingDirection": drivingDirection,
     }
 
+def get_agent_feature(car_group_df, tracks_df, merged_num):
+    ego_id = car_group_df["id"].values[0]
+    frames = car_group_df["frame"].values[::int(merged_num)]
+    # 因为frames过长，不像nuplan每一个场景为固定的时间长度，我们需要把过长的frames拆分成不同的场景，每个场景为10秒，采用移动窗口的方法。
+    #
+    valid_now = frames[25:len(frames)-101]
+    valid_frames = [ frames[i-25:i+101] for i in range(25, len(frames)-101)]
+    test = valid_frames[-1][25]
+    for f in frames:
+        # 获取tracks_df中frame为f的数据
+        frame_f = tracks_df[tracks_df["frame"] == f]
+        print(1)
 
 def main():
     MinT = 10 # 取最少存在10s的車輛才能作為ego car
     FrameRate = 25
-    min_frame = MinT/(1/FrameRate) + 1  # 10s * 25fps = 250 frames
+    merged_frame = 0.08 # 合并后的帧间隔
 
+    # "加"是要保证冗余，加"2"是因为后续要取0.08(2帧合并成1帧)，252 = 2 + 250（250 = 25*2 + 100*2）
+    min_frame = MinT/(1/FrameRate) + 2  # 10s * 25fps = 250 frames
+    merged_num = merged_frame/(1/FrameRate)
     for i in range(60):
         recordingMeta = f"./data/{i+1:02}_recordingMeta.csv"
         tracks = f"./data/{i+1:02}_tracks.csv"
@@ -80,8 +95,8 @@ def main():
                     continue
                 # 取car_df中car_df为frame_group的数据
                 car_group_df = car_df[car_df["frame"].isin(frame_group)]
-                ego_feature = get_ego_feature(car_group_df, dict_cars)
-
+                ego_feature = get_ego_feature(car_group_df, dict_cars, merged_num)
+                get_agent_feature(car_group_df, tracks_df, merged_num)
                 print(1)
 
 
